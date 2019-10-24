b7bd52fd9d643bf92a094037d5bf941bf97da4d1
evandro.alessi@sistemairis.com.br <evandro.alessi@sistemairis.com.br>
2019-10-24 16:31:21 -0300

    'autoload'

--- View/Home/Index.php
+++ View/Home/Index.php
@@ -1,6 +1,16 @@
 <?php 
     require('../../DAO/AeronaveDAO.php');
 
+    spl_autoload_register(
+        function($class)
+        {
+            $folders = ['Controllers', 'Models', 'crossCutting', 'Views'];
+            foreach($folders as $folder)
+                if(file_exists($folder . '/' . $class . '.php'))
+                    require_once($folder . '/' . $class . '.php'));
+        }
+    );
+    
     $aux = new AeronaveDAO();
     $a =  $aux->buscarTodos();
 
